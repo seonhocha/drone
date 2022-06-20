be3d6c4c666df59320536361c193b91f719c1af8
seonhocha <chaseonho1@naver.com>
2022-06-20 16:41:08 +0900

    very very important

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/chaseonho1/drone/src
+# Build directory: /home/chaseonho1/drone/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("yoonsewon_description")
---
+++ build/yoonsewon_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/chaseonho1/drone/src/yoonsewon_description
+# Build directory: /home/chaseonho1/drone/build/yoonsewon_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
